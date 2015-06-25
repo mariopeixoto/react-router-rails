a686996fb0f0d0a03042ab0e8f73209ba84eecc8
Mario Peixoto <mario.peixoto@gmail.com>
2015-06-25 00:18:52 -0300

    removing not need components

--- examples/react-router-example/app/assets/javascripts/components/Admin.js.jsx
+++
@@ -1,9 +0,0 @@
-/** @jsx React.DOM */
-
-var HelloWorld = React.createClass({
-  render: function() {
-    return (
-      <div>Hello world</div>
-    );
-  }
-});
--- examples/react-router-example/app/assets/javascripts/components/website.jsx
+++
@@ -1,9 +0,0 @@
-/** @jsx React.DOM */
-
-var AnotherPage = React.createClass({
-  render: function() {
-    return (
-      <div>Another Page</div>
-    );
-  }
-});
